fee642b3b0b829f8f1c1a1b880a5836426f219c6
Gøran A. Slettemark <goran.slettemark@highsoft.com>
2023-02-05 06:28:55 +0100

    initial commit

---
+++ main.ts
@@ -0,0 +1,26 @@
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+// Load config from .env files
+const env = config({safe: true});
+
+
+const options = {
+  method: "POST",
+  headers: {
+    Authorization: "Bearer " + env.XATA_API_KEY,
+    "Content-Type": "application/json",
+  },
+  body: '{"columns":["*","dose.*"],"page":{"size":15}}',
+};
+
+const response =
+  await (fetch(
+    "https://goransle-s-workspace-0544ur.eu-west-1.xata.sh/db/databetus:main/tables/databetus/query",
+    options,
+  )
+    .then((response) => response.json())
+    .then((response) => JSON.stringify(response))
+    .catch((err) => console.error(err)));
+
+serve((req: Request) => new Response(response));
